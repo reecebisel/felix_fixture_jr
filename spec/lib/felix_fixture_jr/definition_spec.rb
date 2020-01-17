eba4dca0d4dc606c6c44050e6e2626754f9cc9fe
Reece Bisel <reecebisel@gmail.com>
2020-01-16 21:07:16 -0700

    psudo code the definition spec

---
+++ spec/lib/felix_fixture_jr/definition_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "./lib/felix_fixture_jr/definition"
+require "./lib/felix_fixture_jr.rb"
+require "spec_helper"
+
+class Totez
+  def self.table_name
+    "totez"
+  end
+end
+
+class TotezDefintion < FelixFixtureJr::Definition; end
+
+RSpec.describe FelixFixtureJr::Definition do
+  context "no gob object and no parent passed" do
+    before do
+      @subject = described_class.new(Totez)
+    end
+
+    it "#slug_prefix" do
+      expect(@subject.slug_prefix).to eq("totez")
+    end
+
+    it "#file" do
+      expect(@subject.file).to eq File.open("../support/totez.yml")
+    end
+
+    it "#fixturize" do
+      expect(@subject.fixturize).to be_present
+      expect(YAML.parse(@subject.fixturize))
+    end
+
+    it "#build" do
+      expect(@subject.fixture_file.size).to eq 0
+      @subject.build
+      expect(@subject.fixture_file.size).to be > 0
+    end
+
+    it "#constant" do
+      expect(@subject.constant).to eq(Totez)
+    end
+  end
+end
