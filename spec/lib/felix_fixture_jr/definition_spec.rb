fd0fbabeee8e49f68b1f2bd5d62d16e80907d3d4
Reece Bisel <reecebisel@gmail.com>
2020-10-12 11:30:43 -0600

    refacotr spec to use new shared example

--- spec/lib/felix_fixture_jr/definition_spec.rb
+++ spec/lib/felix_fixture_jr/definition_spec.rb
@@ -2,6 +2,7 @@
 
 require "./lib/felix_fixture_jr/definition"
 require "./lib/felix_fixture_jr.rb"
+require "support/shared_definition"
 require "spec_helper"
 
 class UserFixture < User
@@ -9,62 +10,19 @@ class UserFixture < User
 end
 
 RSpec.describe UserFixture do
-  context "minimal setup for class" do
-    before do
-      3.times do |i|
-        User.create(
-          name: "name_#{i}",
-          email: "totez_#{i}@example.com",
-          admin: i % 2 == 0 ? true : false
-        )
-      end
-
-      FelixFixtureJr.configure do |config|
-        config.fixture_directory = "./spec/support"
-      end
-
-      @subject = described_class.new
+  before do
+    3.times do |i|
+      User.create(
+        name: "name_#{i}",
+        email: "totez_#{i}@example.com",
+        admin: i % 2 == 0 ? true : false
+      )
     end
 
-    after do
-      File.write(@subject.file.path, "", mode: "w")
-      User.destroy_all
-    end
-
-    it "#slug_prefix" do
-      expect(@subject.slug_prefix).to eq("users")
-    end
-
-    it "#file" do
-      expect(
-        FileUtils.identical?(@subject.file, File.open("./spec/support/users.yml"))
-      ).to be true
-    end
-
-    it "#fixtures" do
-      expect(@subject.fixtures.length).to eq(3)
-
-      @subject.fixtures.each do |fixture|
-        expect(YAML.parse(fixture))
-      end
-    end
-
-    it "#build" do
-      expect(@subject.file_size).to eq 0
-      @subject.build
-      expect(@subject.file_size).to be > 0
-    end
-
-    it "#build creates new fixture file" do
-      File.delete(@subject.file)
-
-      expect(@subject.file_size).to eq 0
-      @subject.build
-      expect(@subject.file_size).to be > 0
-    end
-
-    it "#constant" do
-      expect(@subject.constant).to eq(User)
+    FelixFixtureJr.configure do |config|
+      config.fixture_directory = "./spec/support"
     end
   end
+
+  it_behaves_like "fixture definition", User, "./spec/support/users.yml"
 end
