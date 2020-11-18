99b2627a695b683c805ae55e4ff37cd1ac62e3d2
Reece Bisel <reecebisel@gmail.com>
2020-11-17 19:49:28 -0700

    fix spacing for rails required files

--- spec/config/environment.rb
+++ spec/config/environment.rb
@@ -1,14 +1,14 @@
 require "rails"
 %w(
-active_record/railtie
-active_storage/engine
-action_controller/railtie
-action_view/railtie
-action_mailer/railtie
-active_job/railtie
-action_cable/engine
-action_mailbox/engine
-rails/test_unit/railtie
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  action_mailbox/engine
+  rails/test_unit/railtie
 ).each do |railtie|
   begin
       require railtie
